92607062e5823a33f5861610e909b4e21ecf3829
Karina Cardoso Telles <karina@divibank.co>
2024-01-20 13:25:15 -0300

    mudanças

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 29;
+let escolha = prompt("Escolha um número entre 1 e 30");
+
+  if(escolha==numeroSecreto){
+    alert("Você acertou!");
+  }
+  else{
+    alert("Você errou!");
+  }
