964cd3c346408dd828a393bad317aab55e109e3d
Karina Cardoso Telles <karina@divibank.co>
2024-01-20 13:33:43 -0300

    adição do template String

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let numeroSecreto = 29;
 let escolha = prompt("Escolha um número entre 1 e 30");
 
   if(escolha==numeroSecreto){
-    alert("Você acertou!");
+    alert("Você acertou! " + `O número secreto é ${numeroSecreto}`);
   }
   else{
     alert("Você errou!");
